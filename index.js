37c123312652fe5f96b3b4fb27340e2a354785b5
Asbjørn Enge <asbjorn@hanafjedle.net>
2015-02-10 23:32:14 +0100

    Added retry-connection and stuffz

--- index.js
+++ index.js
@@ -1,10 +1,41 @@
+var assign  = Object.assign || require('object.assign')
 var request = require('request')
+var retry   = require('retry-connection') 
+var EE      = require('events').EventEmitter
 
 var StatestoreAPI = function(options) {
+    this.ready    = false
     this.host     = options.host
     this.port     = options.port
+    this.interval = options.interval || 5000
+    this.timeout  = options.timeout  || 500
 }
-StatestoreAPI.prototype = {
+StatestoreAPI.prototype = assign({
+
+    listen : function() {
+        this.connection = retry({ 
+            host     : this.host, 
+            port     : this.port,
+            interval : this.interval,
+            timeout  : this.timeout
+        })
+        this.connection.on('ready', this.handleReady.bind(this))
+        this.connection.on('issue', this.handleIssue.bind(this))
+        this.connection.connect()
+    },
+
+    handleReady : function() {
+        if (this.ready) return
+        this.ready  = true
+        this.emit('up')
+    },
+
+    handleIssue : function(issue) {
+        console.log(issue.message)
+        if (!this.ready) return
+        this.ready  = false
+        this.emit('down')
+    },
 
     getState : function(state, callback) {
         request(this.getURI()+state, function(error, response, body) {
@@ -18,6 +49,6 @@ StatestoreAPI.prototype = {
         return 'http://'+this.host+':'+this.port
     },
 
-}
+},EE.prototype)
 
 module.exports = function(options) { return new StatestoreAPI(options)  }
